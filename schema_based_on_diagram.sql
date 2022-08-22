9056c62c926a3afa66bb4419dee75b2ede189625
Mohammed Raad Zahraw Al-Suwayyid <mohamedraad77@gmail.com>
2022-08-22 15:48:31 +0300

    Create indexes and invoices & invoice items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,4 +1,3 @@
-
 CREATE DATABASE medical_clinic;
 
 CREATE TABLE patients (
@@ -27,3 +26,27 @@ CREATE TABLE treatment_histories (
     CONSTRAINT medical_history_fk FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id),
     CONSTRAINT treatment_fk FOREIGN KEY(treatment_id) REFERENCES treatments(id)
 );
+
+CREATE INDEX medical_history_idx ON treatment_histories(medical_history_id);
+CREATE INDEX treatment_idx ON treatment_histories(treatment_id);
+
+CREATE TABLE invoices(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    total_amount DECIMAL NOT NULL,
+    generated_at TIMESTAMP NOT NULL,
+    payed_at TIMESTAMP,
+    medical_history_id INT NOT NULL,
+    CONSTRAINT medical_history_fk FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
+);
+
+CREATE TABLE invoice_items(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    unit_price DECIMAL NOT NULL,
+    quantity INT NOT NULL,
+    total_price DECIMAL NOT NULL,
+    invoice_id INT NOT NULL,
+    treatment_id INT NOT NULL,
+    CONSTRAINT invoice_fk FOREIGN KEY(invoice_id) REFERENCES invoices(id),
+    CONSTRAINT treatment_fk FOREIGN KEY(treatment_id) REFERENCES treatments(id)
+);
+
